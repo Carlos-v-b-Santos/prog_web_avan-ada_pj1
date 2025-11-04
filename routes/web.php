83ef3eb109afddfa56a0294426b7aafcc357f3b0
Carlos V. B. Dos Santos <121064459+Carlos-v-b-Santos@users.noreply.github.com>
2025-11-04 13:10:29 -0400

    versão ultra mega quase final

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,11 @@ use App\Http\Controllers\MensagemController;
 Route::get('/', [SiteController::class, 'home'])->name('home');
 Route::get('/sobre', [SiteController::class, 'sobre'])->name('sobre');
 Route::get('/catalogo/index', [GameController::class, 'catalogo'])->name('catalogo');
-Route::resource('mensagens', MensagemController::class)->only(['index', 'create', 'store']);
+Route::resource('mensagens', MensagemController::class);
 
 Route::middleware('auth')->group(function () {
     Route::resource('games', GameController::class);
+    Route::resource('mensagens', MensagemController::class)->only(['index']);
 });
 
 require __DIR__.'/auth.php';
